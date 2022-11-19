896e94eb66efc8c1806b52b0e8e8e84bd8e681fd
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-19 17:17:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int opzione;
+    float a=0, b=0, area=0;
+    cout<<"Dimmi i valori di a e di b:\n";
+    cin>>a>>b;
+    cout<<"Dimmi l'opzione che scegli:\n";
+    cin>>opzione;
+    switch(opzione) {
+        case 0:
+            area = a * b / 2;
+            cout << area;
+            break;
+        case 1:
+            area = a * a;
+            cout << area;
+            break;
+        case 2:
+            area = a * b;
+            cout << area;
+            break;
+        default:
+            cout << "Opzione non valida";
+    }
+    return 0;
 }
